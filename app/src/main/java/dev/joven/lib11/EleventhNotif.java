c7d9517790db2c2f396a6c07eae5ad52d4aa579d
Blast-Zone-Out <113412493+Blast-Zone-Out@users.noreply.github.com>
2023-12-06 14:37:07 +0800

    Finished

---
+++ app/src/main/java/dev/joven/lib11/EleventhNotif.java
@@ -0,0 +1,21 @@
+package dev.joven.lib11;
+
+
+
+import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.util.Log;
+        import android.widget.Toast;
+
+public class EleventhNotif extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String title = intent.getStringExtra("title");
+        String message = intent.getStringExtra("message");
+
+        //Log.i("mgdNotification", "Received Notification: " + title + " - " + message);
+
+        Toast.makeText(context, "Received Notification: " + title + " - " + message, Toast.LENGTH_SHORT).show();
+    }
+}
